95b0edf5a86b5c45775804f10a5b5ed1cb0de440
rougejaw <61851922+rougejaw@users.noreply.github.com>
2023-04-16 04:03:36 -0400

    implement k means algorithm
    improve sampling using k means algorithm to force sampling over a range of clusters

---
+++ colmap/frame_sampling.py
@@ -0,0 +1,67 @@
+import json
+import sys
+import numpy as np
+import math
+import sklearn.cluster
+import random
+
+if __name__ =='__main__':
+    input_file = sys.argv[1]
+
+    input_str = open(input_file)
+    input = json.loads(input_str.read())
+
+    CLUSTERS = 100
+
+    extrins = []
+    angles = []
+    for f in input["frames"]:
+        extrinsic = np.array(f["extrinsic_matrix"])
+        extrins+=[ extrinsic ]
+    for i,e in enumerate(extrins):
+
+        # t == rectangular coordinates
+        t = e[0:3,3]
+
+        # s == spherical coordinates
+
+        # r = sqrt(x^2 + y^2 + z^2)
+        r = math.sqrt((t[0]*t[0])+(t[1]*t[1])+(t[2]*t[2]))
+        theta = math.acos(t[2]/r)
+        phi = math.atan(t[1]/t[0])
+
+        #convert radian to degrees
+
+        theta = (theta * 180) / math.pi
+        phi = (phi * 180) / math.pi
+
+        s = [theta,phi]
+
+        angles.append(s)
+
+    km = sklearn.cluster.k_means(X=angles, n_clusters=CLUSTERS, n_init=10)
+
+    seen_numbers=[]
+    for i in km[1]:
+        if (i not in seen_numbers):
+            seen_numbers.append(i)
+
+    #TODO account for this
+    if (len(seen_numbers) != CLUSTERS):
+        print("TOO FEW CLUSTERS")
+
+    cluster_array = [ [] for _ in range(CLUSTERS) ]
+    return_array = []
+
+    print(len(angles))
+
+
+    for i in range(len(angles)):
+        cluster_array[km[1][i]].append(i)
+        print(cluster_array[km[1][i]])
+
+    #TODO instead of being completely random, take the point closest to the centroid
+    for i in range(len(cluster_array)):
+        return_array.append(cluster_array[i][random.randint(0,len(cluster_array[i])-1)])
+
+    print(return_array)
